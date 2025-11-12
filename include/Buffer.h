8c5b43442c4ae031a355ad6a0e86a1271bb80a10
flyfeathergodcore <935621414@qq.com>
2025-11-12 18:31:58 +0800

    for_http

--- include/Buffer.h
+++ include/Buffer.h
@@ -14,25 +14,13 @@ public:
           readerIndex_(kCheapPrepend),
           writerIndex_(kCheapPrepend) {}
 
-    size_t readableBytes() const
-    {
-        return writerIndex_ - readerIndex_;
-    }
+    // 返回可读字节数、可写字节数、预留字节数
+    size_t readableBytes() const { return writerIndex_ - readerIndex_; }
+    size_t writableBytes() const { return buffer_.size() - writerIndex_; }
+    size_t prependableBytes() const {return readerIndex_;}
 
-    size_t writableBytes() const
-    {
-        return buffer_.size() - writerIndex_;
-    }
-
-    size_t prependableBytes() const
-    {
-        return readerIndex_;
-    }
-
-    const char *peek() const
-    {
-        return begin() + readerIndex_;
-    }
+    // 返回可读数据的起始地址
+    const char *peek() const { return begin() + readerIndex_;}
 
     void retrieve(size_t len)
     {
@@ -97,18 +85,36 @@ public:
         writerIndex_ += len;
     }
 
-    char *beginWrite()
+    // 返回可写数据的起始地址
+    char *beginWrite() { return begin() + writerIndex_; }
+    const char *beginWrite() const {return begin() + writerIndex_;}
+
+    //find\r\n
+    const char* findCRLF() const
     {
-        return begin() + writerIndex_;
+        const char* crlf = std::search(peek(), begin() + writerIndex_,
+                                       "\r\n", "\r\n" + 2);
+        return crlf == begin() + writerIndex_ ? nullptr : crlf;
     }
 
-    const char *beginWrite() const
+    //find\r\n from start
+    const char* findCRLF(const char* start) const
     {
-        return begin() + writerIndex_;
+        const char* crlf = std::search(start, begin() + writerIndex_,
+                                       "\r\n", "\r\n" + 2);
+        return crlf == begin() + writerIndex_ ? nullptr : crlf;
     }
 
+    // retrieve until end
+    void retrieveUntil(const char* end)
+    {   
+        assert(peek() <= end);
+        assert(end <= beginWrite());
+        retrieve(end - peek()); 
+    }
+
+    //读写文件描述符封装
     ssize_t readFd(int fd, int *savedErrno);
-
     ssize_t writeFd(int fd, int *savedErrno);
 
 private:
